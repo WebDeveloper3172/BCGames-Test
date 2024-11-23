1f5973fe10612ffb82e3f1042b8ab54df65d5ba0
chirilcristina <chirilcristina2002@gmail.com>
2024-11-23 09:34:39 +0200

    I added the Slide to the Player and the pickups like alcohol and money

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider; // Referință la slider-ul din UI (atașat în Canvas-ul player-ului)
+    public int score = 0; // Scorul inițial al jucătorului
+    private void Start()
     {
-        
+        // Inițializează slider-ul cu valoarea scorului
+        if (slider != null)
+        {
+            slider.maxValue = 200; // Poți ajusta valoarea maximă după cum dorești
+            slider.minValue = 0;   // Valoarea minimă
+            slider.value = score;  // Setează scorul inițial
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Alcohol"))
+        {
+            // Scade scorul
+            score -= 10;
+            UpdateSlider();
+            Destroy(other.gameObject); // Distruge obiectul (sticlă de alcool)
+        }
+        else if (other.CompareTag("Money"))
+        {
+            // Crește scorul
+            score += 10;
+            UpdateSlider();
+            Destroy(other.gameObject); // Distruge obiectul (bani)
+        }
     }
+
+    private void UpdateSlider()
+    {
+        // Actualizează valoarea slider-ului
+        slider.value = score;
+    }
+
 }
